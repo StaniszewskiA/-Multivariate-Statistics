2f9880f1676e81727584a0ee0dafb22eeb44d4cc
AStaniszewski <staniszewski01adam@gmail.com>
2024-06-14 13:11:31 +0200

    Lab 2 clear

--- lab2/lab2.r
+++ lab2/lab2.r
@@ -91,7 +91,7 @@ print(train)
 
 ```{r}
 dir_log_t <- list()
-dir_log_t$fit <- glm(Type_of_Renewable_Energy_1 ~ Installed_Capacity_MW + Energy_Production_MWh, 
+dir_log_t$fit <- glm(Type_of_Renewable_Energy_1 ~ Jobs_Created, 
                    family = binomial, data = train)
 summary(dir_logistic$fit)
 ```
@@ -102,33 +102,86 @@ dir_log_t$predicted <- ifelse(dir_log_t$probs > 0.5, "Type 1", "Not type 1")
 table(dir_log_t$predicted, Type_of_Renewable_Energy_1_test)
 ```
 ```{r}
-table(dir_log_best2$predicted, Type_of_Renewable_Energy_1_test)
+mean(dir_logistic$predicted == energy_data$Type_of_Renewable_Energy_1)
 ```
 
 ```{r}
-dir_log_best2$fit <- glm(Type_of_Renewable_Energy_1 ~ Energy_Production_MWh + Energy_Storage_Capacity_MWh, 
+dir_log_best$fit <- glm(Type_of_Renewable_Energy_1 ~ Energy_Production_MWh + Energy_Storage_Capacity_MWh, 
                          family = binomial, 
                     data = energy_data, 
                     subset = train)
 
 ```
 
+```{r prepare data}
+set.seed(123) 
+train_indices <- sample(1:nrow(energy_data), 0.7 * nrow(energy_data))
+train <- rep(FALSE, nrow(energy_data))
+train[train_indices] <- TRUE
+
+test <- !train
+test <- energy_data[test, ]
+
+print(test)
+```
 ```{r lda}
 dir_lda <- list()
-dir_lda$fit <- lda(Type_of_Renewable_Energy_1 ~ Energy_Storage_Capacity_MWh + Energy_Production_MWh,
-                   data = energy_data, subset = train)
-dir_lda$fit
+dir_lda$fit <- lda(Type_of_Renewable_Energy_1 ~ Energy_Production_MWh + Energy_Storage_Capacity_MWh, data = energy_data, subset = train)
+
+print(dir_lda$fit)
+
+dir_lda$predicted <- predict(dir_lda$fit, test)
+predicted_classes <- dir_lda$predicted$class
+actual_classes <- test$Type_of_Renewable_Energy_1
+num_errors <- sum(predicted_classes != actual_classes)
+
+total_samples <- length(actual_classes)
+proportion_of_errors <- num_errors / total_samples
+
+print(proportion_of_errors)
 ```
 
 ```{r qda}
-dir_qda$predicted <- predict(dir_qda$fit, energy_data_test)
-table(dir_qda$predicted$class, Type_of_Renewable_Energy_1_test)
+dir_qda <- list()
+dir_qda$fit <- qda(Type_of_Renewable_Energy_1 ~ Energy_Production_MWh + Energy_Storage_Capacity_MWh, data = energy_data, subset = train)
+
+print(dir_qda$fit)
+
+dir_qda$predicted <- predict(dir_qda$fit, test)
+predicted_classes <- dir_qda$predicted$class
+
+actual_classes <- test$Type_of_Renewable_Energy_1
+num_errors <- sum(predicted_classes != actual_classes)
+total_samples <- length(actual_classes)
+
+proportion_of_errors <- num_errors / total_samples
+print(proportion_of_errors)
+
+```
+```{r}
+install.packages('class')
 ```
 
 ```{r knn}
+library(class)
+
 train_set <- energy_data[train, c("Energy_Storage_Capacity_MWh", "Energy_Production_MWh")]
 test_set <- energy_data[!train, c("Energy_Storage_Capacity_MWh", "Energy_Production_MWh")]
 renewable_energy_train <- energy_data$Type_of_Renewable_Energy_1[train]
-dir_knn_1 <- knn(train_set, test_set, renewable_energy_train, k = 1)
-table(dir_knn_1, Type_of_Renewable_Energy_1_test)
+Type_of_Renewable_Energy_1_test <- energy_data$Type_of_Renewable_Energy_1[!train]
+
+error_proportions <- numeric(10)
+
+for (k in 1:10) {
+  dir_knn <- knn(train_set, test_set, renewable_energy_train, k = k)
+  num_errors <- sum(dir_knn != Type_of_Renewable_Energy_1_test)
+  total_samples <- length(Type_of_Renewable_Energy_1_test)
+  error_proportions[k] <- num_errors / total_samples
+}
+
+print(error_proportions)
+
+plot(1:10, error_proportions, type = "b", xlab = "k", ylab = "Proportion of Errors",
+     main = "Proportion of Errors for KNN with Different k Values")
+
 ```
